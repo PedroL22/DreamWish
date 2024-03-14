2acbdc3ec7593ccd791e221bbb7008f5e7f79d7f
Pedro Lucena <pedrocontact22@gmail.com>
2024-03-13 22:53:31 -0300

    🔥 chore: Remove DB code

--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-import { env } from '../env' // not using alias import because of ts-node in db:migrate script
-
-export const client = postgres(env.EXPO_PUBLIC_DATABASE_URI)
-
-export const db = drizzle(client)
