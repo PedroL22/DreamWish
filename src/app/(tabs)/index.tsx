61f0cac752b899ef4793793217a8a7d4a5529033
Pedro Lucena <pedrocontact22@gmail.com>
2024-03-16 12:17:18 -0300

    ✨💅🏻 feat: Add profile component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { View } from 'tamagui'
-import { Banner } from '~/components/home'
+import { Banner, Profile } from '~/components/home'
 
 export default function HomeScreen() {
   return (
@@ -8,6 +8,14 @@ export default function HomeScreen() {
       alignItems='center'
     >
       <Banner />
+
+      <View
+        width='100%'
+        px='$6'
+        py='$4'
+      >
+        <Profile />
+      </View>
     </View>
   )
 }
